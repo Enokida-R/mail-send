19dc9952d9bfdc60831c6875acf6d39f1ff39f38
Enokida-R <raita.5006@gmail.com>
2024-02-17 23:42:23 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import nodemailer from 'nodemailer';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());//JSONボディのパースを有効化
+
+app.use(express.static('public'));
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.MY_ADDRESS,
+        pass: process.env.MY_PASS,
+    },
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+app.post('/api/send-email', async (req, res) => {
+    const { email, message } = req.body;
+    try {
+        await transporter.sendMail({
+            from: email,
+            to: process.env.MY_ADDRESS,
+            subject: 'メールフォームからのメッセージ',
+            text: message,
+        });
+        res.status(200).json({message: 'メールを送信しました'});
+    } catch (error) {
+        console.error('メール送信エラー', error);
+        res.status(500).json({ message: 'メールの送信に失敗しました'});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
